2d2ee2ce81b2b0ab43a084bc0bda8ed240f02cad
Suryadev <suryadev@pop-os.localdomain>
2023-10-03 07:31:34 +0530

    clean up ignored files

--- client/src/pages/cloudinaryConfig.js
+++
@@ -1,6 +0,0 @@
-import { Cloudinary } from "@cloudinary/url-gen";
-
-// Create a Cloudinary instance with your Cloudinary cloud name
-const cld = new Cloudinary({ cloud: { cloudName: "dnxpue9kq" } });
-
-export default cld;
